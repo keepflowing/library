65b8761d6b780800f2f1fbe61d71f7e4b6ae4206
keepflowing <132106900+keepflowing@users.noreply.github.com>
2023-08-28 15:34:11 +0200

    Play with classes (on a plane)

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
+// const baseUrl = "https://covers.openlibrary.org/b/isbn/"
 const myLibrary = [];
 
-function Book(title, author, year) {
-    return {title, author, year}
+class Book {
+    constructor(title, author, year) {
+      this.title = title;
+      this.author = author;
+      this.year = year;
+    }
+
+    static getAuthor(book) {
+      return book.author;
+    }
+
+    get title() {
+      return this._title;
+    }
+
+    set title(val) {
+      if (val.length < 1) {
+        alert("Title needs to be atleast one character.")
+        return;
+      }
+      this._title = val;
+    }
 }
 
-function addBookToLibrary() {
-  // do stuff here
+class ImportantBook extends Book {
+  constructor(title, author, year) {
+    super(title, author, year);
+  }
 }
 
-let theHobbit = new Book("The Hobbit", "J.R.R Tolkien", 1936)
+function addBookToLibrary(book, library) {
+  library.push(book)
+}
 
-myLibrary.push(theHobbit)
+//9780547928227
+let theHobbit = new ImportantBook("The Hobbit", "J.R.R Tolkien", 1936)
+addBookToLibrary(theHobbit, myLibrary);
